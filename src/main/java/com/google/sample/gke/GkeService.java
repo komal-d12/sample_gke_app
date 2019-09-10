e5a50de2f135ec216ea6f10777295ca276787a75
Komal Dhuri <komal.dhuri@citiustech.com>
2019-09-10 16:14:11 +0530

    Corrected return value

--- src/main/java/com/google/sample/gke/GkeService.java
+++ src/main/java/com/google/sample/gke/GkeService.java
@@ -22,6 +22,6 @@ public class GkeService {
     	strBuffer.append(calendar.get(Calendar.SECOND));
     	strBuffer.append(".");
     	System.out.println(strBuffer.toString());
-        return "";
+        return strBuffer.toString();
     }
 }
